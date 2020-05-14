78831929d534ba2af30ed6cb97354b30dab4d781
Yun Chung <yun.chung@mac.com>
2020-05-14 22:02:56 +0900

    Cocktails App is Fully-Functional

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "creating ingredients..."
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+puts "there are three ingredients now..."
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+
+array = user.flatten
+array = array[1]
+
+array.each do |hash|
+  p Ingredient.create(name: "#{hash["strIngredient1"]}")
+end
+# Ingredient.create(name: "#{drinks['bio']}")
+
+# puts "#{user['name']} - #{user['bio']}"

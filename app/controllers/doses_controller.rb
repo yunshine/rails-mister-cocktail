89afd5836453b2c01d1ceeb0da2474c710d23a61
Yun Chung <yun.chung@mac.com>
2020-05-15 11:43:09 +0900

    Started Heroku

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -17,7 +17,7 @@ class DosesController < ApplicationController
       end
   end
 
-    def destroy
+  def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
     redirect_to cocktail_path(@dose.cocktail)
